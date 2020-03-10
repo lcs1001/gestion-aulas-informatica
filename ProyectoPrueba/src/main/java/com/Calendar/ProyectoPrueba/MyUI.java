9614bfaa1edb9834a6b2596705beca380560e736
Lisa Cané Sáiz <lisa.cane.saiz@gmail.com>
2020-03-10 21:03:35 +0100

    Código para probar Google Calendar API
    Código para obtener y mostrar en pantalla los eventos del Google Calendar de la cuenta asociada a las credenciales (credentials.json).

--- ProyectoPrueba/src/main/java/com/Calendar/ProyectoPrueba/MyUI.java
+++ ProyectoPrueba/src/main/java/com/Calendar/ProyectoPrueba/MyUI.java
@@ -1,14 +1,35 @@
-package com.Calendar.ProyectoPrueba;
+package com.Calendar.ProyectoPrueba;	
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar.Events.CalendarImport;
+import com.google.api.services.calendar.model.Events;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -19,25 +40,95 @@ import com.vaadin.ui.VerticalLayout;
  * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
  * overridden to add component to the user interface and initialize non-component functionality.
  */
+@SuppressWarnings("serial")
 @Theme("mytheme")
 public class MyUI extends UI {
+	
+	private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = CalendarImport.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
+    	
+    	// Build a new authorized API client service.
+        NetHttpTransport HTTP_TRANSPORT;
+        Calendar service;
+        // List the next 10 events from the primary calendar.
+        DateTime now = new DateTime(System.currentTimeMillis());
+        Events events;
         
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
-
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        
-        setContent(layout);
+		try {
+			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+			service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+			        .setApplicationName(APPLICATION_NAME)
+			        .build();
+			
+			events = service.events().list("primary")
+			        .setMaxResults(10)
+			        .setTimeMin(now)
+			        .setOrderBy("startTime")
+			        .setSingleEvents(true)
+			        .execute();
+			
+			List<com.google.api.services.calendar.model.Event> items = events.getItems();
+			
+			VerticalLayout layout = new VerticalLayout();
+			
+	        
+	        if (items.isEmpty()) {
+	            Label label = new Label("No se han encontrado eventos");
+	            layout.addComponent(label);
+	            setContent(layout);
+	        } else {
+	        	Label label1 = new Label("Eventos");
+	        	layout.addComponent(label1);
+	            for (com.google.api.services.calendar.model.Event event : items) {
+	                DateTime start = ((com.google.api.services.calendar.model.Event) event).getStart().getDateTime();
+	                if (start == null) {
+	                    start = event.getStart().getDate();
+	                }
+	                Label label2 = new Label(event.getSummary() + " " + start + " ");
+	                layout.addComponent(label2);
+	            }
+	            setContent(layout);
+	        }		
+			
+		} catch (GeneralSecurityException | IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
