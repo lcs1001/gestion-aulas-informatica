20195205e54cc974fe6a4729d9f0e32b6c084804
Lisa Cané Sáiz <lisa.cane.saiz@gmail.com>
2020-06-03 19:01:18 +0200

    Actualización del script de generación de tablas de la base de datos

--- sql/Script_GestionAulasInformatica.sql
+++ sql/Script_GestionAulasInformatica.sql
@@ -1,88 +1,88 @@
-DROP TABLE IF EXISTS public."PropietarioAula" CASCADE;
+DROP TABLE IF EXISTS public."propietario_aula" CASCADE;
 
-CREATE TABLE public."PropietarioAula" (
-	"idPropietarioAula" character varying(20),
-	"nombrePropietarioAula" character varying(50) NOT NULL,
-	"nombreResponsable" character varying(20) NOT NULL,
-	"apellidosResponsable" character varying(20) NOT NULL,
-	"correoResponsable" character varying(20) NOT NULL,
-	"telefonoResponsable" character varying(20) NOT NULL,
-	"tipo" character varying(15) NOT NULL,
-	CONSTRAINT "PK_PropietarioAula" PRIMARY KEY ("idPropietarioAula"),	
-    CONSTRAINT "UNQ_PropietarioAula_NombrePropietarioAula" UNIQUE ("nombrePropietarioAula"),
-	CONSTRAINT "UNQ_PropietarioAula_NombreApellidosResponsable" UNIQUE ("nombreResponsable", "apellidosResponsable"),
-	CONSTRAINT "UNQ_PropietarioAula_CorreoResponsable" UNIQUE ("correoResponsable"),
-    CONSTRAINT "UNQ_PropietarioAula_TelefonoResponsable" UNIQUE ("telefonoResponsable"),
-	CONSTRAINT "CHK_PropietarioAula_Tipo" CHECK("tipo" IN ('Centro', 'Departamento'))
+CREATE TABLE public."propietario_aula" (
+	id_propietario_aula character varying(20),
+	nombre_propietario_aula character varying(50) NOT NULL,
+	nombre_responsable character varying(20) NOT NULL,
+	apellidos_responsable character varying(20) NOT NULL,
+	correo_responsable character varying(20) NOT NULL,
+	telefono_responsable character varying(20) NOT NULL,
+	tipo character varying(15) NOT NULL,
+	CONSTRAINT "PK_PopietarioAula" PRIMARY KEY (id_propietario_aula),	
+    CONSTRAINT "UNQ_PopietarioAula_NombrePropietarioAula" UNIQUE (nombre_propietario_aula),
+	CONSTRAINT "UNQ_PopietarioAula_NombreApellidosResponsable" UNIQUE (nombre_responsable, apellidos_responsable),
+	CONSTRAINT "UNQ_PopietarioAula_CorreoResponsable" UNIQUE (correo_responsable),
+    CONSTRAINT "UNQ_PopietarioAula_TelefonoResponsable" UNIQUE (telefono_responsable),
+	CONSTRAINT "CHK_PopietarioAula_Tipo" CHECK(tipo IN ('Centro', 'Departamento'))
 );
 
-DROP TABLE IF EXISTS public."Aula" CASCADE;
+DROP TABLE IF EXISTS public."aula" CASCADE;
 
-CREATE TABLE public."Aula" (
-	"nombreAula" character varying(20),
-	"ubicacionCentro" character varying(20) NOT NULL,
-    "propietarioAula" character varying(20) NOT NULL,
-    "capacidad" integer NOT NULL,
-    "numOrdenadores" integer NOT NULL,
-    "disponible" boolean NOT NULL,
-    CONSTRAINT "PK_Aula" PRIMARY KEY ("nombreAula", "ubicacionCentro"),
-    CONSTRAINT "FK_Aula_PropietarioAula_UbicacionCentro" FOREIGN KEY ("ubicacionCentro")
-        REFERENCES public."PropietarioAula" ("idPropietarioAula") 
+CREATE TABLE public."aula" (
+	nombre_aula character varying(20),
+	ubicacion_centro character varying(20) NOT NULL,
+    propietario_aula character varying(20) NOT NULL,
+    capacidad integer NOT NULL,
+    num_ordenadores integer NOT NULL,
+    disponible boolean NOT NULL,
+    CONSTRAINT "PK_Aula" PRIMARY KEY (nombre_aula, ubicacion_centro),
+    CONSTRAINT "FK_Aula_PopietarioAula_UbicacionCentro" FOREIGN KEY (ubicacion_centro)
+        REFERENCES public."propietario_aula" (id_propietario_aula) 
         ON UPDATE CASCADE 
         ON DELETE CASCADE,
-    CONSTRAINT "FK_Aula_PropietarioAula_Propietario" FOREIGN KEY ("propietarioAula")
-        REFERENCES public."PropietarioAula" ("idPropietarioAula")
+    CONSTRAINT "FK_Aula_PopietarioAula_Propietario" FOREIGN KEY (propietario_aula)
+        REFERENCES public."propietario_aula" (id_propietario_aula)
         ON UPDATE CASCADE 
         ON DELETE CASCADE,
-    CONSTRAINT "CHK_Aula_Capacidad" CHECK("capacidad" > 0),
-    CONSTRAINT "CHK_Aula_NumOrdenadores" CHECK("numOrdenadores" > 0)
+    CONSTRAINT "CHK_Aula_Capacidad" CHECK(capacidad > 0),
+    CONSTRAINT "CHK_Aula_NumOrdenadores" CHECK(num_ordenadores > 0)
 );
 
-DROP TABLE IF EXISTS public."Reserva" CASCADE;
+DROP TABLE IF EXISTS public."reserva" CASCADE;
 
-CREATE TABLE public."Reserva" (
-	"idReserva" integer,
-    "fechaInicio" date NOT NULL,
-	"fechaFin" date,
-    "horaInicio" time without time zone NOT NULL,
-    "horaFin" time without time zone NOT NULL,
-    "nombreAula" character varying(20) NOT NULL,
-    "ubicacionCentro" character varying(20) NOT NULL,
-    "diaSemana" character varying(10),
-    "motivo" character varying(50) NOT NULL,
-    "aCargoDe" character varying(50) NOT NULL,
-    "responsable" character varying(20) NOT NULL,
-    "reservaRango" boolean NOT NULL,
-    CONSTRAINT "PK_Reserva" PRIMARY KEY ("idReserva"),
-    CONSTRAINT "FK_Reserva_Aula_AulaCentro" FOREIGN KEY ("nombreAula", "ubicacionCentro")
-        REFERENCES public."Aula"
+CREATE TABLE public."reserva" (
+	id_reserva integer,
+    fecha_inicio date NOT NULL,
+	fecha_fin date,
+    hora_inicio time without time zone NOT NULL,
+    hora_fin time without time zone NOT NULL,
+    nombre_aula character varying(20) NOT NULL,
+    ubicacion_centro character varying(20) NOT NULL,
+    dia_semana character varying(10),
+    motivo character varying(50) NOT NULL,
+    a_cargo_de character varying(50) NOT NULL,
+    responsable character varying(20) NOT NULL,
+    reserva_rango boolean NOT NULL,
+    CONSTRAINT "PK_Reserva" PRIMARY KEY (id_reserva),
+    CONSTRAINT "FK_Reserva_Aula_AulaCentro" FOREIGN KEY (nombre_aula, ubicacion_centro)
+        REFERENCES public."aula"
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    CONSTRAINT "FK_Reserva_PropietarioAula_Responsable" FOREIGN KEY ("responsable")
-        REFERENCES public."PropietarioAula" ("idPropietarioAula") 
+    CONSTRAINT "FK_Reserva_PropietarioAula_Responsable" FOREIGN KEY (responsable)
+        REFERENCES public."propietario_aula" (id_propietario_aula)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    CONSTRAINT "CHK_Reserva_DiaSemana" CHECK("diaSemana" IN ('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado','Domingo')),
-    CONSTRAINT "CHK_Reserva_RangoFechas" CHECK(("reservaRango" IS FALSE AND "diaSemana" IS NULL) OR ("reservaRango" IS TRUE AND "diaSemana" IS NOT NULL))
+    CONSTRAINT "CHK_Reserva_DiaSemana" CHECK(dia_semana IN ('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado','Domingo')),
+    CONSTRAINT "CHK_Reserva_RangoFechas" CHECK((reserva_rango IS FALSE AND dia_semana IS NULL) OR (reserva_rango IS TRUE AND dia_semana IS NOT NULL))
 );
 
-DROP TABLE IF EXISTS public."HistoricoReservas" CASCADE;
+DROP TABLE IF EXISTS public."historico_reservas" CASCADE;
 
-CREATE TABLE public."HistoricoReservas" (
-    "idReserva" integer NOT NULL,    
-    "tipoOperacion" character varying(10) NOT NULL,
-    "fechaOperacion" timestamp without time zone NOT NULL,
-    "responsableOperacion" character varying(20) NOT NULL,
-    CONSTRAINT "PK_HistoricoReservas" PRIMARY KEY ("idReserva", "tipoOperacion"),
-    CONSTRAINT "FK_HistoricoReservas_Reserva" FOREIGN KEY ("idReserva")
-        REFERENCES public."Reserva" ("idReserva") 
+CREATE TABLE public."historico_reservas" (
+    id_reserva integer NOT NULL,    
+    tipo_operacion character varying(10) NOT NULL,
+    fecha_operacion timestamp without time zone NOT NULL,
+    responsable_operacion character varying(20) NOT NULL,
+    CONSTRAINT "PK_HistoricoReservas" PRIMARY KEY (id_reserva, tipo_operacion),
+    CONSTRAINT "FK_HistoricoReservas_Reserva" FOREIGN KEY (id_reserva)
+        REFERENCES public."reserva" (id_reserva) 
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT "FK_HistoricoReservas_PropietarioAula_Responsable" FOREIGN KEY ("responsableOperacion")
-        REFERENCES public."PropietarioAula" ("idPropietarioAula") 
+    CONSTRAINT "FK_HistoricoReservas_PropietarioAula_Responsable" FOREIGN KEY (responsable_operacion)
+        REFERENCES public."propietario_aula" (id_propietario_aula)
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT "UNQ_HistoricoReservas" UNIQUE ("idReserva", "tipoOperacion", "fechaOperacion", "responsableOperacion")    
+    CONSTRAINT "UNQ_HistoricoReservas" UNIQUE (id_reserva, tipo_operacion, fecha_operacion, responsable_operacion)    
 );
 
 COMMIT;
