6b5ed3a1af82ed8410e697f802017155d8151405
Lisa Cané Sáiz <lisa.cane.saiz@gmail.com>
2020-06-30 16:16:59 +0200

    Actualización AppShell
    Eliminada la anotación @PWA

--- src/main/java/gestionaulasinformatica/AppShell.java
+++ src/main/java/gestionaulasinformatica/AppShell.java
@@ -1,13 +1,7 @@
 package gestionaulasinformatica;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.server.PWA;
 
-/**
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- */
-@PWA(name = "Gestión de Aulas de Informática", shortName = "Gestión Aulas Informática")
 public class AppShell implements AppShellConfigurator {
 	private static final long serialVersionUID = 1L;
 }
